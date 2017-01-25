1e03e25d1d8f6fab5b043319b158b3e221c2fffc
Alexander Maltsev <a_maltsev@ngs.ru>
2017-01-25 17:17:06 +0700

    IR indication

--- cgi-bin/request.py
+++ cgi-bin/request.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python2
 import cgi
 import serial
+from random import random
+
+DEBUG = True
 
 form = cgi.FieldStorage()
 
 print("Content-type: text/html\n")
+
+def debug(request):
+    response = 'n'
+    if request == 'A':
+        response = chr(80)
+    if request in ('1', '2', '3', '4', '5', '6'):
+        response = chr(int(random() * 255))
+    if request in ('F', 'B', 'L', 'R', 'S'):
+        response = request
+    return response
 
 def send_request(cmd):
     ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
@@ -16,8 +29,11 @@ def send_request(cmd):
 
 if "cmd" in form:
     cmd = form["cmd"].value
-    try:
-        send_request(cmd)
-    except Exception as e:
-        print(str(e))
+    if DEBUG:
+        print(debug(cmd))
+    else:
+        try:
+            send_request(cmd)
+        except Exception as e:
+            print(str(e))
 
