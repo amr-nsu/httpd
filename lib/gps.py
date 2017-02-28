5013f2caad5fe1628097df6946a41967b919a3c4
Alexander Maltsev <a_maltsev@ngs.ru>
2017-02-28 12:00:12 +0700

    Angle wrap to PI

--- lib/gps.py
+++ lib/gps.py
@@ -1,4 +1,4 @@
-from math import sin, cos
+from math import sin, cos, pi
 
 class GPS:
 
@@ -8,7 +8,17 @@ class GPS:
         self.q = q
 
     def move(self, v, w, dt):
-        self.q = self.q + w * dt
+        self.q = wrap_to_pi(self.q + w * dt)
         self.x = self.x + v * cos(self.q) * dt
         self.y = self.y + v * sin(self.q) * dt
         return self.x, self.y, self.q
+
+
+_2PI = 2. * pi
+
+def wrap_to_pi(angle_rad):
+    while angle_rad > pi:
+        angle_rad -= _2PI
+    while angle_rad < -pi:
+        angle_rad += _2PI
+    return angle_rad
