ad7e63918b91878efcfe2a8a747cdc3f4ea5b9dc
Alexander Maltsev <a_maltsev@ngs.ru>
2017-03-09 12:31:42 +0700

    String command for mcu

--- lib/fserial.py
+++ lib/fserial.py
@@ -8,8 +8,8 @@ class SerialDevice:
         self.serial = serial.Serial(device, baud, timeout=1)
 
     def send_request(self, request):
-        self.serial.write(request)
-        response = self.serial.read()
+        self.serial.write('%s\n' % request)
+        response = self.serial.readline()[:-1]
         if not response:
             response = 'NULL'
         elif request in ('A', '1', '2', '3', '4', '5', '6'):
