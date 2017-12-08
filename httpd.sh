dfee3eac072a1eb8733c59c7828b040c2816a25d
Alex Maltsev <alex.s.maltsev@yandex.ru>
2017-12-08 14:15:25 +0700

    mobile ui

--- httpd.sh
+++ httpd.sh
@@ -4,7 +4,8 @@ cd ${0%/*}
 
 python3 ../stream/stream.py &
 
-../stream/video0.sh &
+#../stream/video0.sh &
 
 #python3 -m http.server --cgi 8000
-python fhttpd.py > /tmp/fhttpd.log
+#python fhttpd.py > /tmp/fhttpd.log
+python fhttpd.py
